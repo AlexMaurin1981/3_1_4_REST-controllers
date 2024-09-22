ce124d99764d4cd5a9c08f85fdf7d7fa1bd9a58d
AlexMaurin1981 <155809065+AlexMaurin1981@users.noreply.github.com>
2024-09-22 15:30:49 +0300

    JS - tableUsers in  adminPanel.html

--- src/main/java/REST_controller/demo/controller/AdminController.java
+++ src/main/java/REST_controller/demo/controller/AdminController.java
@@ -6,6 +6,8 @@ import REST_controller.demo.entetie.User;
 import REST_controller.demo.service.RoleService;
 import REST_controller.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -26,10 +28,9 @@ public class AdminController {
     }
 
     @GetMapping("/users")
-
-    public List<User> showAllUsers() {
-
-        return userService.getAllUsers();
+    public ResponseEntity<List<User>> showAllUsers() {
+        List <User>user=userService.getAllUsers();
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
     @PutMapping("/users")
     public User updateUser (@RequestBody User user) {
